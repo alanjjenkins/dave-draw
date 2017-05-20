bd921686e39461a03e7915bfe65ced53a405406c
Alan Jenkins <alan.james.jenkins@gmail.com>
2017-05-20 17:28:46 +0100

    Implemented weighted random.

--- dave-draw.py
+++ dave-draw.py
@@ -22,6 +22,7 @@ class Viewer(object):
         entrance_message_type,
         entrance_sfx
         ):
+
         self.viewer_id = viewer_id
         self.twitch_name = twitch_name
         self.beam_name = beam_name
@@ -39,16 +40,140 @@ class Viewer(object):
         self.entrance_message_type = entrance_message_type
         self.entrance_sfx = entrance_sfx
 
+    def win_chance(self, total_tickets):
+        """
+        Takes the total tickets (points) as a paramter and works
+        out the percentage chance that the viewer has of winning.
+
+        Returns the viewers win chance in percent.
+        """
+
+        percent = total_tickets / 100.00
+
+        return self.points2 / percent
+
 class DaveDraw(object):
     def __init__(self):
+        self.debug = False
         self.database_path = 'Viewers3DB.sqlite'
         self.db_conn = sqlite3.connect(self.database_path)
+        self.get_viewers()
+        self.calculate_total_points()
+        self.assign_tickets()
+
+    def assign_tickets(self):
+        """
+        Assigns each user a number range based on the number of
+        tickets they have.
+
+        e.g.
+        10 1-10
+        10 11-20
+        30 21-50
+        1  51
+        """
+
+        self.tickets = {}
+        latest_ticket = 0
+
+        for viewer in self.viewers:
+            # skip anyone with no points
+            if viewer.points2 == 0:
+                next
+
+            ticket_range_beg = latest_ticket + 1
+            ticket_range_end = latest_ticket + 1 + viewer.points2
+            latest_ticket = ticket_range_end
+
+            viewer.tickets = range(ticket_range_beg, ticket_range_end)
+
+            # assign a range of tickets:
+            if self.debug:
+                print("Assigning viewer twitch: %s beam: %s tickets %i-%i" % (viewer.twitch_name, viewer.beam_name, viewer.tickets.start, viewer.tickets.stop))
+            if ticket_range_beg == ticket_range_end:
+                if self.debug:
+                    print("Assigning ticket {} to {}".format(ticket_range_beg, viewer.twitch_name))
+                self.tickets[ticket_range_beg] = viewer
+                next
+
+            for ticket in viewer.tickets:
+                if self.debug:
+                    print("Assigning ticket {} to {}".format(ticket, viewer.twitch_name))
+                self.tickets[ticket] = viewer
+
+    def calculate_total_points(self):
+        """
+        Gets the total amount of points awarded to all
+        viewers.
+        """
+
+        self.total_points = 0
+        for viewer in self.viewers:
+            self.total_points += viewer.points2
+
+        self.total_points_percent = self.total_points / 100
+
+        print("Total points awarded (total tickets): %s" % self.total_points)
+
+    def draw(self):
+        """
+        Picks a random number between 1 and total tickets, finds
+        the user that has been assigned tickets within that range and
+        returns the user.
+        """
+
+        ticket = random.randint(1, self.total_points)
+
+        try:
+            winner = self.tickets[ticket]
+        except:
+            pdb.set_trace()
+
+        print("\n===== WINNER Twitch: {} / Beam: {} =====\n".format(winner.twitch_name, winner.beam_id))
+        print("Picked ticket {}\n".format(ticket))
+        print("Winner win chance: {:f}".format(winner.win_chance(self.total_points)))
+        print("Winner's ticket range: {}-{}".format(winner.tickets.start, winner.tickets.stop))
+        print("Winner's ticket amount: {}\n".format(winner.points2))
+
+        self.display_viewer(winner)
+
+    def display_random_viewer(self):
+        """
+        Displays random viewer.
+        """
+        self.display_viewer(self.get_random_viewer())
+
+    def display_viewer(self, viewer):
+        """
+        Outputs the data on all viewers.
+        """
+        print("""Viewer ID: %s\nTwitch Name: %s\nBeam Name: %s\nBeam ID: %s\nRank: %s\nPoints: %s\nPoints2: %s\nHours: %s\nRaids: %s\nGains Currency: %s\nGains Hours: %s\nInGiveaways: %s\nLastSeen: %s\nEntrance Message: %s\nEntranceMsgType: %s\nEntranceSFX: %s"""
+            % (
+                viewer.viewer_id,
+                viewer.twitch_name,
+                viewer.beam_name,
+                viewer.beam_id,
+                viewer.rank,
+                viewer.points,
+                viewer.points2,
+                viewer.hours,
+                viewer.raids,
+                viewer.gains_currency,
+                viewer.gains_hours,
+                viewer.in_giveaways,
+                viewer.last_seen,
+                viewer.entrance_message,
+                viewer.entrance_message_type,
+                viewer.entrance_sfx
+            )
+        )
 
     def get_random_viewer(self):
         """
         Gets a completely random viewer.
         """
         return random.choice(self.viewers)
+
     def get_viewers(self):
         """
         Gets data on all the viewers in the database and stores
@@ -88,49 +213,6 @@ class DaveDraw(object):
                     )
                 )
 
-    def display_random_viewer(self):
-        """
-        Displays random viewer.
-        """
-        self.display_viewer(self.get_random_viewer())
-    def display_viewer(self, viewer):
-        """
-        Outputs the data on all viewers.
-        """
-        print("""Viewer ID: %s\nTwitch Name: %s\nBeam Name: %s\nBeam ID: %s\nRank: %s\nPoints: %s\nPoints2: %s\nHours: %s\nRaids: %s\nGains Currency: %s\nGains Hours: %s\nInGiveaways: %s\nLastSeen: %s\nEntrance Message: %s\nEntranceMsgType: %s\nEntranceSFX: %s"""
-            % (
-                viewer.viewer_id,
-                viewer.twitch_name,
-                viewer.beam_name,
-                viewer.beam_id,
-                viewer.rank,
-                viewer.points,
-                viewer.points2,
-                viewer.hours,
-                viewer.raids,
-                viewer.gains_currency,
-                viewer.gains_hours,
-                viewer.in_giveaways,
-                viewer.last_seen,
-                viewer.entrance_message,
-                viewer.entrance_message_type,
-                viewer.entrance_sfx
-            )
-        )
-
-    def calculate_total_points(self):
-        """
-        Gets the total amount of points awarded to all
-        viewers.
-        """
-
-        self.total_points = 0
-        for viewer in self.viewers:
-            self.total_points += viwer.points2
-
-        print("Total points awarded: %s" % self.total_points)
-
 if __name__ == '__main__':
     dd = DaveDraw()
-    dd.get_viewers()
-    dd.display_random_viewer()
+    dd.draw()
